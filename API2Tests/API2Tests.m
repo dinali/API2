f268c56d07fc495534b8da9a04016a9abfaa0a96
Dina Li <dinali@ers.usda.gov>
2013-01-23 09:36:52 -0500

    Initial Commit

---
+++ API2Tests/API2Tests.m
@@ -0,0 +1,32 @@
+//
+//  API2Tests.m
+//  API2Tests
+//
+//  Created by Dina Li on 1/23/13.
+//  Copyright (c) 2013 USDA ERS. All rights reserved.
+//
+
+#import "API2Tests.h"
+
+@implementation API2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in API2Tests");
+}
+
+@end
